121299eb01a88e92b90be1cd0fd14dd23043092d
Jack Oliver <jackoliver1993@yahoo.co.uk>
2026-03-06 15:36:40 +0000

    huggingFace model repo download added

---
+++ KYLE_Py/app/huggingFaceModels.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from huggingface_hub import hf_hub_download, snapshot_download
+
+BASE_DIR = Path(__file__).resolve().parent
+
+# Download entire repository
+model_dir = snapshot_download(
+    repo_id="jollyOli93/Models",
+    local_dir=BASE_DIR / "Models"
+)
